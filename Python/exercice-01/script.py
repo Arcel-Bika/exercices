c7efa56eb6c9dc071e1a381a3bc8e22dc4abf51b
Arcel-Bika <arcelbika@gmail.com>
2025-01-26 22:53:57 +0100

    exercice-01-python

---
+++ Python/exercice-01/script.py
@@ -0,0 +1,14 @@
+"""
+Global Tech - Python - Exercice 01
+"""
+
+user: dict = {"name": "John", "age": 30, "city": ..., "email": "john@exemple.com"}
+
+print(all(user.values()))
+
+# Answer: 
+
+# A. True
+# B. False
+# C. None
+# D. Error
