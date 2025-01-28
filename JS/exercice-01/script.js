9e87bd22aa4d5ee404b9b880a169ce1ba5ba26a1
Arcel-Bika <arcelbika@gmail.com>
2025-01-29 00:04:16 +0100

    exercice-01-js

---
+++ JS/exercice-01/script.js
@@ -0,0 +1,16 @@
+//
+// Global Tech - JavaScript - Exercice 01
+//
+
+let a = 5,
+    b = 3,
+    c = 2;
+
+let result = a + b + c;
+console.log(result);
+
+// Answer: 
+// A. Error
+// B. 10
+// C. 532
+// D. 5 + 3 + 2
