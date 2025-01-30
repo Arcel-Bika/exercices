c10b0d80175810e2b93b77679ff06c58e5d3cd2c
Arcel-Bika <arcelbika@gmail.com>
2025-01-30 09:28:17 +0100

    exercice-02-JS

---
+++ JS/exercice-02/script.js
@@ -0,0 +1,19 @@
+//
+// Global Tech - JavaScript - Exercice 02
+//
+
+let x = 15;
+let result;
+if (x > 10) {
+  result = "Grand";
+} else {
+  result = "Petit";
+}
+
+console.log(result);
+
+// Answer:
+// A. "Grand"
+// B. "Petit"
+// C. true
+// D. false
