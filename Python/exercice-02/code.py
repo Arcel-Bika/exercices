4e1bd382f4db8bfe52a369a2b740abd72377798b
Arcel-Bika <arcelbika@gmail.com>
2025-02-03 08:57:04 +0100

    exercice-02-python

---
+++ Python/exercice-02/code.py
@@ -0,0 +1,13 @@
+"""
+Global Tech - Python - Exercice 01
+"""
+
+my_list = [1, 2, 3]
+my_list.extend([4, 5, 6])
+
+# Answer: 
+
+# A. [1, 2, 3, [4, 5, 6]]
+# B. [1, 2, 3]
+# C. [1, 2, 3, 4, 5, 6]
+# D. Error
