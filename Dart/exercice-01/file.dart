bee89eafb9a3ca28f715147b498bd0c54c133d52
Arcel-Bika <arcelbika@gmail.com>
2025-02-07 22:19:23 +0100

    exercice-01-dart

---
+++ Dart/exercice-01/file.dart
@@ -0,0 +1,18 @@
+//
+// Global Tech - Dart - Exercice 01
+//
+
+void main() {
+  var year = 2024;
+
+  while (year != 2025) {
+    print("Nous sommes en $year");
+    year++;
+  }
+}
+
+// Answer: 
+// A. Nous sommes en 2024
+// B. Nous sommes en 2025
+// C. Error
+// D. Nous sommes en 2024\nNous sommes en 2025
