93438ec675be367af0d22ae041e115934ab10787
Arcel-Bika <arcelbika@gmail.com>
2025-02-13 09:56:01 +0100

    exercice-05-JS

---
+++ JS/exercice-05/script.js
@@ -0,0 +1,13 @@
+//
+// Global Tech - JavaScript - Exercice 05
+//
+
+let table = [1, 2, 3, 4, 5];
+let result = table.fill(0);
+console.log(result);
+
+// Answer:
+// A. [0, 1, 2, 3, 4]
+// B. [1, 2, 3, 4, 5]
+// C. [0, 0, 0, 0, 0]
+// D. [0]
