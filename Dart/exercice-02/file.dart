8ec26dafbaa1714f372a382fbbac7d2b960f31a0
Arcel-Bika <arcelbika@gmail.com>
2025-02-14 10:00:55 +0100

    exercice-02-Dart

---
+++ Dart/exercice-02/file.dart
@@ -0,0 +1,15 @@
+//
+// Global Tech - Dart - Exercice 02
+//
+
+void main() {
+  var voyager = ("I'm leaving on a trip soon on", DateTime(1977, 9, 5));
+  print(voyager);
+}
+
+// Answer:
+
+// A. I'm leaving on a trip soon on 1977-09-05 00:00:00.000
+// B. I'm leaving on a trip soon on 1977-09-05
+// C. I'm leaving on a trip soon on 1977
+// D. Error
