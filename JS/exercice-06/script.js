961127d142d6e9cfb1b9ff78247c8ed9f1c6bd37
Arcel-Bika <arcelbika@gmail.com>
2025-02-19 09:13:24 +0100

    exercice-06-JS

---
+++ JS/exercice-06/script.js
@@ -0,0 +1,13 @@
+//
+// Global Tech - JavaScript - Exercice 06
+//
+
+let numbers = [1, 2, 3, 4, 5];
+let result = numbers.filter(num => num > 3);
+
+// Answer:
+
+// A. [4, 5]
+// B. [1, 2, 3]
+// C. [1, 2, 3, 4, 5]
+// D. [3, 4, 5]
