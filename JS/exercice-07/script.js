b5c232aefd28feaa37d1b78aafc0f9eaef0d94a6
Arcel-Bika <arcelbika@gmail.com>
2025-02-20 11:12:24 +0100

    exercice-07-JS

---
+++ JS/exercice-07/script.js
@@ -0,0 +1,16 @@
+//
+// Global Tech - JavaScript - Exercice 07
+//
+
+const x = [5];
+const y = x == false;
+const z = x === false;
+
+console.log(z);
+
+// Answer:
+
+// A. true
+// B. false
+// C. undefined
+// D. error
